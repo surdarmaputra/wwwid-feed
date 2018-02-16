56df6ef8de225a72f7380a60b735edf5dfe9f980
sdarmaputra <surya.igede@gmail.com>
2018-02-16 16:51:40 +0700

    Webpack setup - setup webpack for SASS - install plugins: html-webpack-plugin, extract-text-webpack-plugin - fix webpack-dev-server startup script due to HMR problem

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const SRC = path.resolve(__dirname, 'src')
 const DIST = path.resolve(__dirname, 'dist')
+
+const generatePage = new HTMLWebpackPlugin({
+	title: 'WWWID READER',
+	template: path.resolve(SRC, 'index.html')
+})
+const extractStyle = new ExtractTextPlugin({
+	filename: 'app.css'
+})
 
 const config = {
 	entry: path.resolve(SRC, 'app.js'),
@@ -15,9 +25,26 @@ const config = {
 		port: 16006,
 		contentBase: SRC,
 		compress: true,
-		hot: true,
 		open: true
-	}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: extractStyle.extract({
+					use: [
+						'css-loader',
+						'sass-loader'
+					],
+					fallback: 'style-loader'
+				})
+			}
+		]
+	},
+	plugins: [
+		generatePage,
+		extractStyle
+	]
 }
 
 module.exports = config
