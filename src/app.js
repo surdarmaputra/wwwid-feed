0786cc599dc5554aa357666b69fcad0d96f68b23
sdarmaputra <surya.igede@gmail.com>
2018-02-17 10:41:40 +0700

    WIP: routing mechanism - create wrapper for router - create sample link to use hash router

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const { create, draw } = require('./utils/dom')
 const style = require('./app.scss')
 const NavBar = require('./components/NavBar')
 const Feed = require('./components/Feed')
+const Article = require('./components/Article')
 
 const store = {
 	appName: 'WWWID Reader',
@@ -11,6 +12,7 @@ const store = {
 			author: 'Me',
 			thumbnail: 'https://cdn-images-1.medium.com/max/939/1*DAksMcGmL2voVF39xrACQA.png',
 			summary: 'This is my first feed.',
+			content: '<p>This is my first feed.</p>',
 			pubDate: new Date()
 		}
 	]
@@ -21,19 +23,81 @@ for (let i = 0; i < 8; i++) {
 	store.feeds.push(example)
 }
 
-const Home = () => {
+const Home = (props) => {
 	const home = create('div')
-	const feeds = store.feeds.map((feed) => {
+	const feeds = props.feeds.map((feed) => {
 		home.append(Feed(feed))
 	})
 	return home
 }
 
+const Detail = (props) => {
+	const detail = Article(props.feeds[0])
+	return detail
+}
+
+const Page = (props) => {
+	const routes = [
+		{
+			location: '/',
+			exact: true,
+			component: Home
+		},
+		{
+			location: '/article',
+			exact: false,
+			component: Detail
+		}
+	]
+	const page = routes.reduce((selected, current) => current.location === props.location ? current : selected, routes[0])
+	return page.component(props)
+}
+
+const withRouter = (component, props) => {
+	const hash = '/#'
+	const wrapper = create('div')
+	const route = getRoute()
+	const processRoute = (route) => {
+		const newProps = Object.assign({}, props, { 
+			location: route
+		})
+		draw(component(newProps), wrapper)
+	}
+	if (location.href.indexOf(hash) === -1) {
+		location.replace(location + '#/')
+	}
+	window.onhashchange = () => {
+		const route = getRoute()
+		processRoute(route)	
+	}
+
+	processRoute(route)
+	return wrapper
+}
+
+const getRoute = () => {
+	return location.hash.substr(1)
+}
+
+const setRoute = (route) => {
+	location.hash = '#' + route
+}
+
 const App = () => {
 	const app = create('div')
+	const sampleLink = create('a', {
+		textContent: 'Article',
+		href: '/article',
+		onclick: (event) => {
+			event.preventDefault()
+			console.log(event)
+			setRoute(event.target.pathname)
+		}
+	})
 	app.append(
 		NavBar({ title: store.appName }),
-		Home()
+		withRouter(Page, store),
+		sampleLink
 	)
 	return app
 }
