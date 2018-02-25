a2f115a6d43da89bb85c4f6ae4ad171eedfc200a
Surya Darma Putra <sdarmaputra@users.noreply.github.com>
2018-02-25 22:26:59 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ const cacheName = 'wwwid'
 const dataCacheName = 'wwwid-feeds'
 const dataSourceUrl = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fwwwid'
 const mainUrl = self.origin
-
+console.log(self)
 const filesToCache = [
 	mainUrl + '/',
 	mainUrl + '/#/',
