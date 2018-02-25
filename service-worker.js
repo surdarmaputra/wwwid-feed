b22e4ee05a6ac402eb7a9d60093b331d48d4f75c
sdarmaputra <surya.igede@gmail.com>
2018-02-25 22:31:59 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,22 @@
 const cacheName = 'wwwid'
 const dataCacheName = 'wwwid-feeds'
 const dataSourceUrl = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fwwwid'
-const mainUrl = self.origin
+const mainUrl = self.registration.scope
+
 console.log(self)
+console.log(mainUrl)
 const filesToCache = [
-	mainUrl + '/',
-	mainUrl + '/#/',
-	mainUrl + '/index.html',
-	mainUrl + '/app.css',
-	mainUrl + '/app.js',
-	mainUrl + '/manifest.json',
-	'https://fonts.googleapis.com/css?family=Roboto:400',
-	mainUrl + '/images/wwwid-192.png',
-	mainUrl + '/images/wwwid-512.png',
-	mainUrl + '/images/wwwid-small.png',
-	mainUrl + '/images/wwwid-text.png'
+	mainUrl,
+	mainUrl + '#/',
+	mainUrl + 'index.html',
+	mainUrl + 'app.css',
+	mainUrl + 'app.js',
+	mainUrl + 'manifest.json',
+	mainUrl + 'images/wwwid-192.png',
+	mainUrl + 'images/wwwid-512.png',
+	mainUrl + 'images/wwwid-small.png',
+	mainUrl + 'images/wwwid-text.png',
+	'https://fonts.googleapis.com/css?family=Roboto:400'
 ]
 
 self.addEventListener('install', function(e) {
